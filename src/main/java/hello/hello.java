eca85bf0ca0fea7a2d9ecdb0cc88b1ca86d67d1b
janani <root@host129.interns.hpe>
2017-03-10 15:27:33 +0530

    third

--- src/main/java/hello/hello.java
+++ src/main/java/hello/hello.java
@@ -1,12 +1,10 @@
 package hello;
-import org.joda.time.LocalTime;
 
 public class hello
 {
  	public static void main(String args[])
 	{
-		LocalTime c = new LocalTime();
-	System.out.println("the time is "+c);
+	System.out.println("the time is ");
 	}
 }
 
